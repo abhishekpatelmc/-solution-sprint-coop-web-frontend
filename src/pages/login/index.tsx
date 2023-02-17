b63619bba71865b8e675320a2317e73d2402501d
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-02-17 01:18:48 -0500

    feat: API endpoint added -- login page added

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
-import { type NextPage } from "next";
+import React from "react";
 import Head from "next/head";
 
-const Login: NextPage = () => {
+const index = () => {
   return (
     <>
       <Head>
@@ -10,17 +10,19 @@ const Login: NextPage = () => {
       <div>
         <div className="flex h-screen items-center justify-center">
           <div className="w-full max-w-md rounded-md bg-white p-8 shadow-md">
-            <h1 className="mb-4 text-2xl font-bold">Login</h1>
+            <h1 className="mb-4 text-center text-2xl font-bold tracking-wider">
+              Login
+            </h1>
             <form>
-              <div className=" mb-4">
+              <div className="mb-4">
                 <label
-                  className="mb-2 block font-bold text-gray-700"
+                  className="mb-2 block font-bold  text-gray-700"
                   htmlFor="username"
                 >
                   Username
                 </label>
                 <input
-                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
+                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700 "
                   type="text"
                   id="username"
                 />
@@ -33,7 +35,7 @@ const Login: NextPage = () => {
                   Email
                 </label>
                 <input
-                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
+                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700"
                   type="email"
                   id="email"
                 />
@@ -46,13 +48,13 @@ const Login: NextPage = () => {
                   Password
                 </label>
                 <input
-                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
+                  className="focus:shadow-outline w-full rounded border py-2 px-3 leading-tight text-gray-700 "
                   type="password"
                   id="password"
                 />
               </div>
               <button
-                className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 "
                 type="submit"
               >
                 Sign In
@@ -65,4 +67,4 @@ const Login: NextPage = () => {
   );
 };
 
-export default Login;
+export default index;
