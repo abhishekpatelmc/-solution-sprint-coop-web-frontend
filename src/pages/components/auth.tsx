593963b287f417c25d09e1223fc17fbe43edc5d8
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-24 03:43:23 -0400

    Bug: build error fixing on auth.tsx

--- src/pages/components/auth.tsx
+++ src/pages/components/auth.tsx
@@ -4,51 +4,51 @@ import Router from "next/router";
 import type { IPublicClientApplication } from "@azure/msal-browser";
 
 // JJ Code Start
-export function signInClickHandler(instance: IPublicClientApplication): void {
-  instance
-    .loginPopup({
+export async function signInClickHandler(
+  instance: IPublicClientApplication
+): Promise<void> {
+  try {
+    const e = await instance.loginPopup({
       scopes: ["User.Read"],
       authority: "https://login.microsoftonline.com/uwindsor.ca",
       prompt: "select_account",
       loginHint: "@uwindsor.ca",
       redirectUri: "http://localhost:3000",
-    })
-    .then((e) => {
-      if (e.account?.username.endsWith("@uwindsor.ca")) {
-        console.log("Logged In : ", e);
-        Router.reload();
-        // const router = useRouter();
-        // router.push("/");
-      } else {
-      }
-    })
-    .catch((e) => {
-      console.log("Error : ", e);
-      instance
-        .handleRedirectPromise()
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
     });
+    if (e.account?.username.endsWith("@uwindsor.ca")) {
+      console.log("Logged In : ", e);
+      Router.reload();
+      // const router = useRouter();
+      // router.push("/");
+    }
+  } catch (e) {
+    console.log("Error : ", e);
+    try {
+      const res = await instance.handleRedirectPromise();
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
-
 export async function signOutClickHandler(
   instance: IPublicClientApplication
 ): Promise<void> {
-  //Jaiman Code
-  const msalInstance = new PublicClientApplication(msalConfig);
+  try {
+    //Jaiman Code
+    const msalInstance = new PublicClientApplication(msalConfig);
 
-  const accounts = msalInstance.getAllAccounts();
-  console.log("Account ::: ", accounts, accounts[0]?.homeAccountId);
-  const homeAccID = accounts[0]?.homeAccountId;
+    const accounts = msalInstance.getAllAccounts();
+    console.log("Account ::: ", accounts, accounts[0]?.homeAccountId);
+    const homeAccID = accounts[0]?.homeAccountId;
 
-  const logoutRequest = {
-    account: instance.getAccountByHomeId(String(homeAccID)),
-    postLogoutRedirectUri: "http://localhost:3000/",
-  };
-  await instance.logoutPopup(logoutRequest);
-  return;
+    const logoutRequest = {
+      account: instance.getAccountByHomeId(String(homeAccID)),
+      postLogoutRedirectUri: "http://localhost:3000/",
+    };
+    await instance.logoutPopup(logoutRequest);
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
