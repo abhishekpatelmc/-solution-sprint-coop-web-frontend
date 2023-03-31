a9f2fa2a2388e11220d9ade765d54420363b1a35
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-31 01:03:15 -0400

    Fix: #37 Added footer

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -46,7 +46,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <div className=" bg-cyan-900 shadow-2xl shadow-slate-500 ">
+      <div className=" bg-slate-900 shadow-2xl shadow-slate-500 ">
         <nav className="mx-14 flex h-20 flex-row items-center justify-between text-xl text-gray-50">
           <div className="flex basis-1/5 items-center gap-1">
             <Image
